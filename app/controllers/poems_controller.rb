7add36dd135c6863b709e077b1c2f6e3fe7b79c6
americanmouths <shiverr@gmail.com>
2017-12-19 19:43:52 -0800

    Create edit routes

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -17,12 +17,12 @@ class PoemController < ApplicationController
   post '/poems' do
     redirect?
 
-    unless Poem.valid_params?(params)
+    unless Poem.valid_params?(params) && Category.valid_params?(params)
       redirect to "/poems/new"
     end
 
     @poem = Poem.create(title: params[:title], date: params[:date], content: params[:content])
-    @poem.category = Category.find_or_create_by(name: params[:category])
+    @poem.category = Category.find_or_create_by(name: params[:name])
     @poem.save
     binding.pry
     redirect to "/poems/#{@poem.id}"
@@ -33,6 +33,30 @@ class PoemController < ApplicationController
     @poem = Poem.find(params[:id])
     erb :'poems/show'
   end
+
+  get '/poems/:id/edit' do
+   redirect?
+     @poem = Poem.find(params[:id])
+     if @poem.user_id = current_user.id
+       erb :'/poems/edit'
+     else
+       redirect to "/poems"
+   end
+ end
+
+ patch '/poems/:id' do
+   redirect?
+   @poem = Poem.find(params[:id])
+
+   unless Poem.valid_params?(params) && Category.valid_params?(params)
+     redirect to "/poems/new"
+   end
+
+   @poem.update(title: params[:title], date: params[:date], content: params[:content])
+   @poem.category = Category.find_or_create_by(name: params[:name])
+   @poem.save
+     redirect to "/poems/#{@poem.id}"
+ end
 
 
 end
