e6987a6be5b4222ea788c6438dee7576895f5722
americanmouths <shiverr@gmail.com>
2017-12-19 21:11:29 -0800

    Add edit and delte routes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -28,4 +28,38 @@ class CategoryController < ApplicationController
     erb :'categories/show'
   end
 
+  get '/categories/:id/edit' do
+  redirect?
+    @category = Category.find(params[:id])
+    if @category.user_id = current_user.id
+      erb :'/categories/edit'
+    else
+      redirect to "/categories"
+  end
+end
+
+post '/categories/:id' do
+  redirect?
+  @category = Category.find(params[:id])
+
+  unless Category.valid_params?(params)
+    redirect to "/categories/new"
+  end
+
+  @category.update(params)
+  @category.poems = params["category"]["poems_id"]
+  @category.save
+    redirect to "/categories/#{@category.id}"
+end
+
+delete '/categories/:id/delete' do
+  @category = Category.find(params[:id])
+  if logged_in? && @category.user_id == current_user.id
+    @category.destroy
+    redirect to "/categories"
+  else
+    redirect "/login"
+  end
+end
+
 end
