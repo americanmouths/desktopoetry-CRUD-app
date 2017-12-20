be95bc22a6f3379f1ce20213a7383faaf7d97992
americanmouths <shiverr@gmail.com>
2017-12-19 19:04:23 -0800

    Add valid params method

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,4 +1,9 @@
 class Poem < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
+
+  def self.valid_params?(params)
+    return !params[:title].empty? || !params[:content].empty? || !params[:date].empty? || !params[:category].empty?
+  end
+  
 end
