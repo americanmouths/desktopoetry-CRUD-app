6a0941f9f7fe71a653f29d82b5566dd7f82833a3
americanmouths <shiverr@gmail.com>
2017-12-19 18:37:36 -0800

    Define helper methods and route to main index

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,32 @@
+require 'rack/flash'
+class ApplicationController < Sinatra::Base
+  use Rack::Flash
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "poetryisthesecret"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+    def redirect?
+      if !logged_in
+        redirect to "/login"
+      end
+    end
+  end
+
+  get '/' do
+    erb :'index'
+  end
+
+end
