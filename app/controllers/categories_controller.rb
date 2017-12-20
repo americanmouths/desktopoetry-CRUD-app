db9b7690a05b8fe8d055bc59dbe818f3aee739cc
americanmouths <shiverr@gmail.com>
2017-12-19 20:03:04 -0800

    Add main route and new routes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,25 @@
 class CategoryController < ApplicationController
+  get '/categories' do
+    redirect?
+    @user = current_user
+    @categories = Category.all
+    erb :"/categories/index"
+  end
+
+  get '/categories/new' do
+    redirect?
+      @categories = Category.all
+      @poems = Poem.all
+      erb :'categories/new'
+  end
+
+  post '/categories' do
+    redirect?
+    unless Category.valid_params?(params)
+      redirect to "/categories/new"
+    end
+    @category = Category.create(params)
+    @category.poems = Poem.find_or_create_by(params["category"]["poems_id"])
+    redirect to "/categories/#{@category.id}"
+    end
 end
