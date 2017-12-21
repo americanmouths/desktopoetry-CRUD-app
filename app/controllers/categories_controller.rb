561ac772866009934b85c49a1041e0729678d71f
americanmouths <shiverr@gmail.com>
2017-12-20 17:58:36 -0800

    Add rack flash errors, update patch edit so current user can only edit their own categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -16,9 +16,14 @@ class CategoryController < ApplicationController
   post '/categories' do
     redirect?
     unless Category.valid_params?(params)
+      flash[:error] = "Please do not leave any fields empty"
       redirect to "/categories/new"
     end
-    @category = Category.create(params)
+    @category = Category.create(params[:category][:name])
+    params[:category][:poems].each do |poem_data|
+      poem = Poem.find_or_create_by(poem_data)
+      poem.category = @category
+      poem.save
     redirect to "/categories/#{@category.id}"
     end
 
@@ -31,26 +36,36 @@ class CategoryController < ApplicationController
   get '/categories/:id/edit' do
   redirect?
     @category = Category.find(params[:id])
-    if @category.user_id = current_user.id
+    if @category.user_id == current_user.id
       erb :'/categories/edit'
     else
       redirect to "/categories"
   end
 end
 
-post '/categories/:id' do
+patch '/categories/:id' do
   redirect?
   @category = Category.find(params[:id])
 
   unless Category.valid_params?(params)
+    flash[:error] = "Please do not leave any fields empty"
     redirect to "/categories/new"
   end
 
-  @category.update(params)
-  @category.poems = params["category"]["poems_id"]
-  @category.save
-    redirect to "/categories/#{@category.id}"
-end
+  if logged_in? && @category.user_id == current_user.id
+      @category.update(params[:category][:name])
+      params[:category][:poems].each do |poem_data|
+        poem = Poem.find_or_create_by(poem_data)
+        poem.category = @category
+        poem.save
+      end
+        @category.save
+        flash[:message] = "The category has been updated"
+        redirect to "/categories/#{@category.id}"
+    else
+      redirect "/login"
+    end
+  end
 
 delete '/categories/:id/delete' do
   @category = Category.find(params[:id])
