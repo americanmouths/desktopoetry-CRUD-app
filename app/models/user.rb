386ef8e69b33f0e4c20687b019dd158f48a8c642
americanmouths <shiverr@gmail.com>
2017-12-20 17:51:37 -0800

    Add method for checking valid params and add has many relationship with categories

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :password, :email
   has_secure_password
   has_many :poems
+  has_many :categories
+
+  def self.valid_params?(params)
+   return !params[:username].empty? && !params[:password].empty? && !params[:email].empty?
+ end
+ 
 end
