3c506463c492f4e5013fe89c08152d5084f9b3a8
americanmouths <shiverr@gmail.com>
2017-12-20 18:35:31 -0800

    define slug and find by slug methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,13 @@ class User < ActiveRecord::Base
   def self.valid_params?(params)
    return !params[:username].empty? && !params[:password].empty? && !params[:email].empty?
  end
- 
+
+ def slug
+   self.username.downcase.gsub(" ", "-")
+ end
+
+ def self.find_by_slug(slug)
+   User.all.find{|user| user.slug == slug}
+ end
+
 end
