27013228dcaa00961e4c52abb7e5a14bcf7e2833
americanmouths <shiverr@gmail.com>
2017-12-20 20:58:48 -0800

    update new method to assign category id to user id

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,14 +2,11 @@ class CategoryController < ApplicationController
   get '/categories' do
     redirect?
     @user = current_user
-    @categories = Category.all
     erb :"/categories/index"
   end
 
   get '/categories/new' do
     redirect?
-      @categories = Category.all
-      @poems = Poem.all
       erb :'categories/new'
   end
 
@@ -19,13 +16,16 @@ class CategoryController < ApplicationController
       flash[:error] = "Please do not leave any fields empty"
       redirect to "/categories/new"
     end
-    @category = Category.create(params[:category][:name])
+    @category = Category.create(name: params[:category][:name])
     params[:category][:poems].each do |poem_data|
       poem = Poem.find_or_create_by(poem_data)
       poem.category = @category
       poem.save
-    redirect to "/categories/#{@category.id}"
     end
+    @category.user_id = current_user.id
+    @category.save
+    redirect to "/categories/#{@category.id}"
+  end
 
   get '/categories/:id' do
     redirect?
@@ -40,8 +40,8 @@ class CategoryController < ApplicationController
       erb :'/categories/edit'
     else
       redirect to "/categories"
+    end
   end
-end
 
 patch '/categories/:id' do
   redirect?
@@ -53,12 +53,12 @@ patch '/categories/:id' do
   end
 
   if logged_in? && @category.user_id == current_user.id
-      @category.update(params[:category][:name])
-      params[:category][:poems].each do |poem_data|
+      @category.update(name: params[:category][:name])
+        params[:category][:poems].each do |poem_data|
         poem = Poem.find_or_create_by(poem_data)
         poem.category = @category
         poem.save
-      end
+    end
         @category.save
         flash[:message] = "The category has been updated"
         redirect to "/categories/#{@category.id}"
@@ -67,15 +67,14 @@ patch '/categories/:id' do
     end
   end
 
-delete '/categories/:id/delete' do
-  @category = Category.find(params[:id])
-  if logged_in? && @category.user_id == current_user.id
-    @category.destroy
-    flash[:message] = "The category has been deleted"
-    redirect to "/categories"
-  else
-    redirect "/login"
+  delete '/categories/:id/delete' do
+    @category = Category.find(params[:id])
+    if logged_in? && @category.user_id == current_user.id
+      @category.destroy
+      flash[:message] = "The category has been deleted"
+      redirect to "/categories"
+    else
+      redirect "/login"
+    end
   end
-end
-
 end
